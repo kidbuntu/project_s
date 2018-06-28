74bb94ce6f168e0741d013d84d07ce49b47f7e6a
kidbuntu <harryinoferio@gmail.com>
2018-06-28 15:29:08 +0800

    events

--- js/event_tab.js
+++ js/event_tab.js
@@ -1,17 +1,28 @@
 $(function(){
+// DATAGRID	
 	$('#dg_event').datagrid({
 		fit:true,
 		title:'Events',
 		pagination:true,
-		toolbar:[{
+		toolbar:'#tb_event',
+		columns:[[
+			{field:'id',title:'Id',width:100},
+			{field:'name',title:'Name',width:100},
+			{field:'category',title:'Category',width:100},
+			{field:'description',title:'Description',width:100},
+			{field:'created_by',title:'Created By',width:100},
+			{field:'created_dt',title:'Created Date',width:100},
+			{field:'start_dt',title:'Start Date',width:100},
+			{field:'last_updated_by',title:'Last Updated By',width:100},
+			{field:'last_updated_dt',title:'Last Updated Date',width:100}
+		]],
+		fitColumns:true
+	});
+
+	// MENUBUTTON
+		$('#mb_new').menubutton({
 			iconCls:'icon-add',
-			text:'Create'
-		},"-",{
-			iconCls:'icon-edit',
-			text:'Edit'
-		},"-",{
-			iconCls:'icon-remove',
-			text:'Remove'
-		}]
-	})
+			menu:'#mm_new'
+		});
+
 });
--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ $(function(){
 	});
 
 	// TAB CONTAINER
-	// $('#tab_container').tabs({
-	// 	fit:true,
-	// 	tabHeight:80,
-	// 	tabWidth:120,
-	// 	// selected:5
-	// });	
+	$('#tab_container').tabs({
+		fit:true,
+		tabHeight:80,
+		tabWidth:120,
+		// selected:5
+	});	
 });
